a7662100fdb9c76a97c7aa59104e061e53f6573d
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-21 16:42:02 -0300

    firstProject

---
+++ ws-csharp/FirstProject/FirstProject/Model/HelloModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject.Model
+{
+    public class HelloModel
+    {
+        public string mensagem1 = "My first hello world in CSHAP";
+
+        public string Mensagem2 { get; set; }
+    }
+}
---
+++ ws-csharp/FirstProject/FirstProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using FirstProject.Model;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var nome = "fiap";
+
+        int valor = 10;
+
+        List<int> lista = new List<int>();
+
+        for (int i = 0; i < valor; i++)
+        {
+            lista.Add(i);
+        }
+
+        lista.ForEach(x => Console.WriteLine(x));
+
+        foreach (var vlr in lista)
+        {
+            Console.WriteLine(vlr);
+        }
+    }
+}
